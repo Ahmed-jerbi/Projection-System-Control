c1d844876f1fd257ca52687bc3c5e90b76d75bc0
Ahmed Jerbi <52385138+Ahmed-jerbi@users.noreply.github.com>
2023-06-19 12:53:59 +0200

    Update dshow_cam_settings.bat
    space handling

--- dshow_cam_settings.bat
+++ dshow_cam_settings.bat
@@ -1,9 +1,9 @@
 :: Shows the dshow camera settings dialogue
-:: takes a camera name as an input argument, e.g. "dshow_cam_settings.bat LogitechAJ"
+:: takes a camera name as an input argument, example: dshow_cam_settings.bat "LogitechAJ"
 :: needs FFMPEG library: https://ffmpeg.org/
 
 @echo off
-ffmpeg -f dshow -show_video_device_dialog true -i video="%1" -hide_banner
+ffmpeg -f dshow -show_video_device_dialog true -i video=%1 -hide_banner
 if %ERRORLEVEL% neq 0 goto ProcessError
 
 ::If the camera name is invalid show the dshow devices list
